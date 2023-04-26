b4cf986a09b3c628e9f3d705387b4e2190653847
ivgo <ivgo@spreadgroup.com>
2023-04-26 14:45:22 +0200

    Enable state display if unifiedAlerting

--- src/components/AlertsCard/AlertsCard.tsx
+++ src/components/AlertsCard/AlertsCard.tsx
@@ -30,6 +30,7 @@ const AlertStatusBadge = ({ alert }: { alert: GrafanaAlert }) => {
 
   switch (alert.state) {
     case "ok":
+    case "Normal":
       statusElmt = <StatusOK />;
       break;
     case "paused":
@@ -37,9 +38,13 @@ const AlertStatusBadge = ({ alert }: { alert: GrafanaAlert }) => {
       break;
     case "no_data":
     case "pending":
+    case "Pending":
+    case "NoData":
       statusElmt = <StatusWarning />;
       break;
     case "alerting":
+    case "Alerting":
+    case "Error":
       statusElmt = <StatusError />;
       break;
     default:
@@ -125,7 +130,7 @@ export const AlertsCard = (opts?: AlertsCardOpts) => {
     return <MissingAnnotationEmptyState annotation={GRAFANA_ANNOTATION_ALERT_LABEL_SELECTOR} />;
   }
 
-  const finalOpts = {...opts, ...{showState: opts?.showState && !unifiedAlertingEnabled}};
+  const finalOpts = {...opts, ...{showState: opts?.showState}};
 
   return <Alerts entity={entity} opts={finalOpts} />;
 };
